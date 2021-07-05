d0a0e9797e3d65dd6144a24b9f7ffd0651f9b1e6
aliabbasekber <ugur_atam@hotmail.com>
2021-07-05 16:47:31 +0300

    05/07/2021 16:47

---
+++ oopIntro/src/oopIntro/Category.java
@@ -0,0 +1,11 @@
+package oopIntro;
+
+public class Category {
+	
+	
+	
+	int id;
+	String name;
+	
+
+}
--- oopIntro/src/oopIntro/Main.java
+++ oopIntro/src/oopIntro/Main.java
@@ -4,15 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Product product1 = new Product(); // referans oluþturma, intance
-		product1.id = 1;
-		product1.name = "Lenovo V14";
-		product1.unitPrice = 15000;
-		product1.detail = "16 GB Ram";
+		Product product1 = new Product(1,"Lenovo V14",15000,"16 GB Ram",10,13500); // referans oluþturma, intance
 		
-		Product[] products= {product1};
+		Product product2 = new Product(); // referans oluþturma, intance
+		product2.setId(2);
+		product2.setName("Lenovo V15");
+		product2.setDetail("16 Gb Ram");
+		product2.setDiscount(10);
+		product2.setUnitPrice(16000);
 		
-		System.out.println(products.length);
+		System.out.println(product2.getUnitPriceAfterDiscount());
+		
 		
 	}
 
